64c091e58e141d40dbbf4f3dbdb452d8cde5d625
Thomas Stensitzki <thomas@sf-tools.net>
2018-04-04 09:44:40 +0200

    AssemblyInfo updated

--- CategoryManager/CategoryManagerCmd/Properties/AssemblyInfo.cs
+++ CategoryManager/CategoryManagerCmd/Properties/AssemblyInfo.cs
@@ -2,36 +2,19 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ManageCategoriesCmd")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CategoryManagerCmd")]
+[assembly: AssemblyDescription("Command line toll to import Exchange mailbox master category list using Exchange Web Services.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ManageCategoriesCmd")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 | Published under MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("40e03355-33ca-4961-b419-d6f01226857c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
